211e367a07614ccf150a0cb55f60b3cf29864708
Bhaskar Neupane <bhaskar.neupane.58@gmail.com>
2021-08-22 14:05:16 +0545

    debug output added for heroku !!

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+import sys
+
 from pages.messenger_features import MessengerFeatures, MoreAdvanceMessengerFeatures
 
+print(sys.version, "=====", sys.version_info)
+
 for i in range(1000):
+    print("====" * 10)
+    print(f"START: {i}")
     MoreAdvanceMessengerFeatures(number_of_threads_to_use=1, total_request_count=10000).run(use_threads=False)
+    print(f"END: {i}")
+    print("====" * 10)
