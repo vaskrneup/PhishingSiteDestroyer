26a3812ca43b1f796ba3e7417dd30f88064d627a
Bhaskar Neupane <bhaskar.neupane.58@gmail.com>
2021-08-22 14:17:17 +0545

    Updated to use threads

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ print(sys.version, "=====", sys.version_info)
 for i in range(1000):
     print("====" * 10)
     print(f"START: {i}")
-    MoreAdvanceMessengerFeatures(number_of_threads_to_use=1, total_request_count=10000).run(use_threads=False)
+    MoreAdvanceMessengerFeatures(number_of_threads_to_use=64, total_request_count=10000).run(use_threads=True)
     print(f"END: {i}")
     print("====" * 10)
